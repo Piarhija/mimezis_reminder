eec0017ca79064df283b5cdf44c4ffd550c621f6
pia <100622201+Piarchy@users.noreply.github.com>
2022-04-04 20:21:14 +0200

    Final version, or is it?

---
+++ action.php
@@ -0,0 +1,144 @@
+<?php
+
+
+$file = "task.json";
+$jsonfile = file_get_contents($file);
+$json_a = json_decode($jsonfile, true);
+
+if (empty($_GET['action'])) {
+	echo"Error: What do you want me to do? \n<br /><a href=\"index.php\">Go back and try again please.</a>";
+} elseif (isset($_GET['action']) && $_GET['action'] == 'edit' ) {
+#toon editformulier 
+	$taskid=htmlspecialchars($_GET['id']);
+	$found=0;
+	echo "<h2>Edit</h2>";
+	foreach ($json_a as $item => $task) {
+		if ($item == $taskid) {
+		$found = 1;
+		echo "<form name=\"edit\" action=\"action.php\" method=\"GET\">";
+		echo "<input name=\"content\" type=\"text\" value=\"";
+		echo $task['task'];
+		echo "\"></input>";
+		echo "<input type=\"hidden\" name=\"id\" value=\"".  $taskid ."\"></input>";		
+		echo "<input type=\"hidden\" name=\"action\" value=\"update\"></input>";
+		echo "<input type=\"submit\" name=\"submit\" value=\"Submit\"></input>";
+		echo "</form>";
+		echo "<p />";
+		}
+	}		
+		
+	if ($found == 0) {
+		echo"Error: Task not found.";
+	} 
+	
+} elseif (isset($_GET['submit']) && $_GET['action'] == 'update' && !empty($_GET['id']) && !empty($_GET['content'])) {
+#update task
+	$taskid=htmlspecialchars($_GET['id']);
+	$value=htmlspecialchars($_GET['content']);
+	foreach ($json_a as $item => $task) {
+		if ($item == $taskid) {
+			$found = 1;
+			$current = file_get_contents($file);
+			$current = json_decode($current, TRUE);
+			$json_a = array($taskid => array("task" => $value, "status" => "open"));
+			$replaced = array_replace_recursive($current, $json_a);
+			$replaced = json_encode($replaced);
+			if(file_put_contents($file, $replaced, LOCK_EX)) {
+				echo"Task updated. You will now be redirected back to the task list.";
+				echo"<a href=\"index.php\">If that does not happen, please click here.</a>";
+				?>
+				<script type="text/javascript">
+				window.location = "index.php"
+				</script>
+				<?php
+			} else {
+				echo"failure.";
+			}
+		}
+	}
+	if ($found==0) {
+		echo "Error. Task not found. <a href=\"index.php\">Go back and try again please..</a>";
+	}
+	
+} elseif (isset($_GET['submit']) && $_GET['action'] == 'add' && !empty($_GET['content'])) {
+	#add task
+	$id=substr(md5(rand()), 0, 20);
+	$value=htmlspecialchars($_GET['content']);	
+	$current = file_get_contents($file);
+	$current = json_decode($current, TRUE);
+	$json_a = array($id => array("task" => $value, "status" => "open"));
+	if(is_array($current)) {
+		$current = array_merge_recursive($json_a, $current);
+	} else {
+		$current = $json_a;
+	}
+	$current=json_encode($current);	
+	if(file_put_contents($file, $current, LOCK_EX)) {
+		echo"The task is added.<br />\nYou will now be redirected back to the task list.<br /> \n";
+		echo"<a href=\"index.php\">If that does not happen, please click here.</a>";
+		?>
+		<script type="text/javascript">
+		window.location = "index.php"
+		</script>
+		<?php
+	} else {
+		echo"failure.";
+	}
+} elseif (isset($_GET['action']) && $_GET['action'] == 'done' && !empty($_GET['id'])) {
+	#task is done
+	$taskid=htmlspecialchars($_GET['id']);
+	foreach ($json_a as $item => $task) {
+		if ($item == $taskid) {
+			$found = 1;
+			$current = file_get_contents($file);
+			$current = json_decode($current, TRUE);
+			$json_a = array($taskid => array("task" => $task['task'], "status" => "closed"));
+			$done = array_replace_recursive($current, $json_a);
+			$done = json_encode($done);
+			if(file_put_contents($file, $done, LOCK_EX)) {
+				echo"The task is marked as done.<br />\nYou will now be redirected back to the task list. <br />\n";
+				echo"<a href=\"index.php\">If that does not happen, please click here.</a>";
+				?>
+				<script type="text/javascript">
+				window.location = "index.php"
+				</script>
+				<?php
+			} else {
+				echo"failure.";
+			}
+		}
+	}
+	if ($found==0) {
+		echo "Error. Task not found. <a href=\"index.php\">return.</a>";
+	}
+} elseif (isset($_GET['action']) && $_GET['action'] == 'delete' && !empty($_GET['id'])) {
+#delete task
+	$taskid=htmlspecialchars($_GET['id']);
+	foreach ($json_a as $item => $task) {
+		if ($item == $taskid) {
+			$found = 1;
+			$current = file_get_contents($file);
+			$current = json_decode($current, TRUE);
+			unset($current[$taskid]);
+			$deleted = json_encode($current);		
+			if(file_put_contents($file, $deleted, LOCK_EX)) {
+				echo"The task is deleted.<br />\nYou will now be redirected back to the task list. ";
+				echo"<a href=\"index.php\">If that does not happen, please click here.</a>";
+				?>
+				<script type="text/javascript">
+					window.location = "index.php"
+				</script>
+				<?php
+			} else {
+				echo"failure.";
+			}
+		}
+	}
+	if ($found==0) {
+		echo "Error. Task not found. <a href=\"index.php\">Please go back to the task list and try again.</a>";
+	}
+} else {
+	echo"What do you want me to do?";
+}	
+
+?>
---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+   $file = file_get_contents("task.json");
+   $json_a = json_decode($file, true);
+   $closed=0;
+   $havetasks = 0;
+   ?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>mimezis_todo</title>
+      <meta name=”robots” content=”noindex”>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="" />
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+ 
+                                       
+      <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+   </head>
+   <body>
+      <div class="wrapper ">
+         <div class="mimezisi">
+            <div class="row" >
+               <div id="sad" style="position:absolute"><img src="1sad.gif"></div>
+               <div id="happy" style="position:absolute" ><img src="1hepi.gif"></div>
+            </div>
+            <div class="mimezisiclick">
+               <a href="index.php" style="positon:absolute"><img src="mimezishappy.jpg"> </a>
+               <a href="index2.php"><img src="mimezishappy.jpg"> </a>
+               <a href="index3.php"><img src="mimezishappy.jpg"> </a>
+               <a href="index4.php"><img src="mimezishappy.jpg"> </a>
+            </div>
+         </div>
+         <br>
+         <div>
+            <?php
+               echo "<form style='margin-left:5px; height= 100px;' name=\"edit\" action=\"action.php\" method=\"GET\">";
+                     
+               echo "<input style='width:89.1%;    border-width: 1px;
+                  border-style: solid;
+                  border-color: pink;
+                  height: 30px;
+                  padding-left:5px;' name=\"content\" type=\"text\" placeholder=\"\" ></input>";
+               echo "<input type=\"hidden\" name=\"action\" value=\"add\"></input>";
+               
+               echo "<input style='float: right; background-color: pink;
+                border-width:1px;
+                border-style: solid;
+                border-color: pink;
+                padding: 5.8px 11px;
+                text-decoration: none;
+                 position: absolute;
+                 margin-left: 5px;
+                
+                cursor: pointer;' type=\"submit\" name=\"submit\" value=\"+\"></input>";
+               echo "</form>";
+                         
+               echo "<div style='margin-left: 5px; list-style-type: none;'>";
+               echo "<ul style=' list-style-type: none;'>";
+               if(is_array($json_a)) {		
+               	foreach ($json_a as $item => $task) {
+               		if ($task['status'] == "open") {	
+               			$havetasks=1;
+               			echo "<li style='margin-top:5px;margin-bottom: 5px; background-color: white; padding-top: 5px; padding-bottom: 7px; border-width:1px;border-style: solid; border-color:pink; padding-left:5px; width: 89.1%;'>";
+               			echo $task['task'];
+               			
+               			echo " <a style='float: right; width:32px; height:30.5px; background-color:pink;margin-top:-6px; 
+                              margin-right: -1px;border-width:1px;border-style: solid; border-color:pink; margin-right:-38px; ' href=\"action.php?id=" .$item. "&action=delete\"></a>  ";
+               			echo "</li>";}	}
+               	if($havetasks > 0) {          
+               echo '<script type="text/JavaScript"> 
+                   document.getElementById("happy").style.display = "none";
+                   </script>';}
+               } else { 
+               	echo"drugi if";	
+               }
+               echo "</ul>";
+               echo "</div>";
+                ?>
+         </div>
+      </div>
+   </body>
+</html>
+
